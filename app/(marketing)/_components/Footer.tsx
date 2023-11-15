d98d346e1c54cbcada9ec170ca5d6f08b8b75cc6
RowdyKGZ <73703199+RowdyKGZ@users.noreply.github.com>
2023-11-15 16:28:38 +0600

    folder structure end start heroes layout

---
+++ app/(marketing)/_components/Footer.tsx
@@ -0,0 +1,9 @@
+import { Logo } from "./Logo";
+
+export const Footer = () => {
+  return (
+    <div className="flex items-center w-full p-6 bg-background z-50">
+      <Logo />
+    </div>
+  );
+};
---
+++ app/(marketing)/_components/Logo.tsx
@@ -0,0 +1,8 @@
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+
+export const Logo = () => {
+  return <div>Logo</div>;
+};
